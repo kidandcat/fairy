164b142f2091c901eae94d50dcff52b166458044
Jairo <kidandcat@gmail.com>
2024-08-18 19:51:45 +0200

    chore: Refactor code to improve readability and maintainability

--- extension.js
+++ extension.js
@@ -2,11 +2,12 @@ const vscode = require('vscode');
 const OpenAI = require('openai');
 const { toFile } = require('openai/uploads')
 const { SpeechRecorder } = require("speech-recorder");
-const fs = require('fs');
 
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
+
+const DEBUG = 'multiplica width y height por dos'
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -39,7 +40,7 @@ function add_lines(content) {
 	// add line numbers to the code
 	var lines = content.split('\n')
 	var numbered_lines = []
-	for (var i = 0; i < lines.length; i++) {
+	for (var i = 1; i <= lines.length; i++) {
 		numbered_lines.push(i.toString() + ' ' + lines[i])
 	}
 	return numbered_lines.join('\n')
@@ -56,106 +57,163 @@ async function run_assistant(filename, content) {
 	var input = await listen_input()
 	messages.push({ role: "user", content: input })
 
-	// string
-	// number
-	// integer
-	// object
-	// array
-	// boolean
-	// null
+
 	const runner = openai.beta.chat.completions.runTools({
 		messages: messages,
 		model: 'gpt-4o-mini',
-		tool_choice: 'required',
+		// tool_choice: 'required',
 		tools: [
-			{
-				type: 'function',
-				function: {
-					name: 'ReplaceCodeAtLine',
-					description: 'Replace code at a specific line',
-					function: ReplaceCodeAtLine,
-					parse: JSON.parse,
-					parameters: {
-						type: 'object',
-						properties: {
-							line: {
-								type: 'number',
-								description: 'Line number to replace',
-							},
-							code: {
-								type: 'string',
-								description: 'Code to replace with',
-							},
-						},
-					},
-				},
-			},
-			{
-				type: 'function',
-				function: {
-					name: 'Save',
-					description: 'Save the file',
-					function: Save,
-					parse: JSON.parse,
-					parameters: {
-						type: 'object',
-						properties: {},
-					},
-				},
-			},
-			{
-				type: 'function',
-				function: {
-					name: 'DeleteLines',
-					description: 'Delete lines',
-					function: DeleteLines,
-					parse: JSON.parse,
-					parameters: {
-						type: 'object',
-						properties: {
-							start: {
-								type: 'number',
-								description: 'Start line number to delete',
-							},
-							end: {
-								type: 'number',
-								description: 'End line number to delete (exclusive)',
-							},
-						},
-					},
-				},
-			},
-			{
-				type: 'function',
-				function: {
-					name: 'FocusLine',
-					description: 'Scroll to a specific line',
-					function: FocusLine,
-					parse: JSON.parse,
-					parameters: {
-						type: 'object',
-						properties: {
-							line: {
-								type: 'number',
-								description: 'Line number to focus on',
-							},
-						},
-					},
-				},
-			},
+			ReplaceCodeAtLine(),
+			Save(),
+			DeleteLines(),
+			FocusLines(),
 		],
 	}).on('functionCall', (functionCall) => console.log('functionCall', functionCall));
 
 	try {
-		await runner.finalChatCompletion();
+		console.log(await runner.totalUsage());
 	} catch (e) {
 		console.log('finalChatCompletion Error:', e);
 	}
 }
 
+///////////
+// TOOLS //
+///////////
+// string
+// number
+// integer
+// object
+// array
+// boolean
+// null
+
+function ReplaceCodeAtLine() {
+	return {
+		type: 'function',
+		function: {
+			name: 'ReplaceCodeAtLine',
+			description: 'Replace code at a specific line',
+			parse: JSON.parse,
+			parameters: {
+				type: 'object',
+				properties: {
+					line: {
+						type: 'number',
+						description: 'Line number to replace',
+					},
+					code: {
+						type: 'string',
+						description: 'Code to replace with',
+					},
+				},
+			},
+			function: async ({ line, code }) => {
+				await vscode.window.activeTextEditor.edit(editBuilder => {
+					const start = new vscode.Position(line, 0)
+					const end = new vscode.Position(line + 1, 0)
+					editBuilder.replace(new vscode.Range(start, end), code + '\n')
+					vscode.window.showInformationMessage("Replaced code at line " + line)
+				})
+				const content = vscode.window.activeTextEditor.document.getText()
+				return vscode.window.activeTextEditor.document.uri + ':\n' + add_lines(content)
+			},
+		},
+	};
+}
+
+function Save() {
+	return {
+		type: 'function',
+		function: {
+			name: 'Save',
+			description: 'Save the file',
+			parse: JSON.parse,
+			parameters: {
+				type: 'object',
+				properties: {},
+			},
+			function: () => new Promise((resolve) => {
+				vscode.window.activeTextEditor.document.save()
+				resolve('Saved file ' + vscode.window.activeTextEditor.document.uri)
+			}),
+		},
+	};
+}
+
+function DeleteLines() {
+	return {
+		type: 'function',
+		function: {
+			name: 'DeleteLines',
+			description: 'Delete lines',
+			parse: JSON.parse,
+			parameters: {
+				type: 'object',
+				properties: {
+					start: {
+						type: 'number',
+						description: 'Start line number to delete',
+					},
+					end: {
+						type: 'number',
+						description: 'End line number to delete (exclusive)',
+					},
+				},
+			},
+			function: ({ start, end }) => new Promise((resolve) => {
+				vscode.window.activeTextEditor.edit(editBuilder => {
+					const startPos = new vscode.Position(start, 0)
+					const endPos = new vscode.Position(end, 0)
+					editBuilder.delete(new vscode.Range(startPos, endPos))
+					vscode.window.showInformationMessage("Deleted lines " + startPos + " to " + endPos)
+					const content = vscode.window.activeTextEditor.document.getText()
+					resolve(vscode.window.activeTextEditor.document.uri + ':\n' + add_lines(content))
+				})
+			}),
+		},
+	};
+}
+
+function FocusLines() {
+	return {
+		type: 'function',
+		function: {
+			name: 'FocusLines',
+			description: 'Focus on a range of lines',
+			parse: JSON.parse,
+			parameters: {
+				type: 'object',
+				properties: {
+					start: {
+						type: 'number',
+						description: 'Start line number to focus',
+					},
+					end: {
+						type: 'number',
+						description: 'End line number to focus (exclusive)',
+					},
+				},
+			},
+			function: ({ start, end }) => new Promise((resolve) => {
+				vscode.window.activeTextEditor.selection = new vscode.Selection(start, 0, end, 0)
+				resolve('Focused on lines ' + start + ' to ' + end)
+			}),
+		},
+	};
+}
+
+
+/////////////////
+// AUDIO INPUT //
+/////////////////
 
 async function listen_input() {
 	return new Promise((resolve) => {
+		if (DEBUG != '') {
+			resolve(DEBUG)
+			return
+		}
 		var data = []
 		const recorder = new SpeechRecorder({
 			consecutiveFramesForSpeaking: 10,
@@ -185,50 +243,6 @@ async function listen_input() {
 		recorder.start()
 	});
 }
-
-async function ReplaceCodeAtLine({ line, code }) {
-	return new Promise((resolve) => {
-		vscode.window.activeTextEditor.edit(editBuilder => {
-			const start = new vscode.Position(line, 0)
-			const end = new vscode.Position(line, 0)
-			editBuilder.replace(new vscode.Range(start, end), code)
-			vscode.window.showInformationMessage("Replaced code at line " + line)
-			const content = vscode.window.activeTextEditor.document.getText()
-			resolve(vscode.window.activeTextEditor.document.uri + ':\n' + add_lines(content))
-		})
-	})
-}
-
-async function DeleteLines({ startLine, endLine }) {
-	return new Promise((resolve) => {
-		vscode.window.activeTextEditor.edit(editBuilder => {
-			const start = new vscode.Position(startLine, 0)
-			const end = new vscode.Position(endLine, 0)
-			editBuilder.delete(new vscode.Range(start, end))
-			vscode.window.showInformationMessage("Deleted lines " + start + " to " + end)
-			const content = vscode.window.activeTextEditor.document.getText()
-			resolve(vscode.window.activeTextEditor.document.uri + ':\n' + add_lines(content))
-		})
-	})
-}
-
-async function FocusLine({ line }) {
-	return new Promise((resolve) => {
-		vscode.window.activeTextEditor.selection = new vscode.Selection(line, 0, line, 0)
-		resolve('Focused on line ' + line)
-	})
-}
-
-async function Save() {
-	return new Promise((resolve) => {
-		vscode.window.activeTextEditor.document.save()
-		resolve('Saved file ' + vscode.window.activeTextEditor.document.uri)
-	})
-}
-
-
-
-// AUDIO
 
 function createWavHeader({ sampleRate, bitDepth, channels, dataLength }) {
 	const header = Buffer.alloc(44);
